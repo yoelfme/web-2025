43356bcea1df2c53455befbf9d1a8a7162dcf1c8
Yoel Monzón <yoelfme@gmail.com>
2025-09-06 16:24:02 -0600

    feat: migrations in nestjs

---
+++ week-8/backend/src/config/typeorm.config.ts
@@ -0,0 +1,18 @@
+import { registerAs } from '@nestjs/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+const config = {
+  type: 'postgres',
+  host: `${process.env.DATABASE_HOST}`,
+  port: `${process.env.DATABASE_PORT}`,
+  username: `${process.env.DATABASE_USERNAME}`,
+  password: `${process.env.DATABASE_PASSWORD}`,
+  database: `${process.env.DATABASE_NAME}`,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/migrations/*{.ts,.js}'],
+  autoLoadEntities: true,
+  synchronize: false,
+};
+
+export default registerAs('typeorm', () => config);
+export const connectionSource = new DataSource(config as DataSourceOptions);
